ef9d8e59eb03c032003ef09ccded21d11791885f
Daniel Weibel <danielmweibel@gmail.com>
2019-10-13 21:30:23 +0200

    Refactoring

--- k8s-aliases.go
+++ k8s-aliases.go
@@ -7,181 +7,181 @@ import (
 
 /* Types */
 
-type Pair struct {
+type Token struct {
 	Short, Long string
 }
 
-type Token []Pair
+type Segment []Token
 
 type Group struct {
-	Tokens            []Token
+	Segments          []Segment
 	AllowCombinations bool
 }
 
-type Set []Group
+type Suite []Group
 
 //==============================================================================
 // Declarative part (define your desired behaviour here)
 //==============================================================================
 
-/* Sets */
+/* Suites */
 
-var sets = []Set{
-	Set{actionsGet, resources, optionsGet},
-	Set{actionsDelete, resources, optionsDelete},
-	Set{actionsDescribe, resources, optionsDescribe},
-	Set{actionsExec, optionsExec},
-	Set{actionsLogs, optionsLogs},
-	Set{actionsEdit, resources},
-	Set{actionsOther},
+var suites = []Suite{
+	Suite{actionsGet, resources, optionsGet},
+	Suite{actionsDelete, resources, optionsDelete},
+	Suite{actionsDescribe, resources, optionsDescribe},
+	Suite{actionsExec, optionsExec},
+	Suite{actionsLogs, optionsLogs},
+	Suite{actionsEdit, resources},
+	Suite{actionsOther},
 }
 
 /* Groups */
 
 var resources = Group{
-	[]Token{resourcePod, resourceDeployment, resourceService, resourceNode,
+	[]Segment{resourcePod, resourceDeployment, resourceService, resourceNode,
 		resourceIngress, resourceRole, resourceRoleBinding, resourceClusterRole,
 		resourceClusterRoleBinding},
 	false,
 }
 
 var actionsGet = Group{
-	[]Token{actionGet},
+	[]Segment{actionGet},
 	false,
 }
 var optionsGet = Group{
-	[]Token{optionWatch, optionOutput, optionAllNamespaces},
+	[]Segment{optionWatch, optionOutput, optionAllNamespaces},
 	true,
 }
 
 var actionsDelete = Group{
-	[]Token{actionDelete},
+	[]Segment{actionDelete},
 	false,
 }
 var optionsDelete = Group{
-	[]Token{optionAllDelete, optionAllNamespacesDelete},
+	[]Segment{optionAllDelete, optionAllNamespacesDelete},
 	true,
 }
 
 var actionsDescribe = Group{
-	[]Token{actionDescribe},
+	[]Segment{actionDescribe},
 	false,
 }
 var optionsDescribe = Group{
-	[]Token{optionAllNamespaces},
+	[]Segment{optionAllNamespaces},
 	true,
 }
 
 var actionsExec = Group{
-	[]Token{actionExec},
+	[]Segment{actionExec},
 	false,
 }
 var optionsExec = Group{
-	[]Token{optionInteractive},
+	[]Segment{optionInteractive},
 	true,
 }
 
 var actionsLogs = Group{
-	[]Token{actionLogs},
+	[]Segment{actionLogs},
 	false,
 }
 var optionsLogs = Group{
-	[]Token{optionFollow},
+	[]Segment{optionFollow},
 	true,
 }
 
 var actionsEdit = Group{
-	[]Token{actionEdit},
+	[]Segment{actionEdit},
 	false,
 }
 
 var actionsOther = Group{
-	[]Token{actionApply, actionPortForward, actionExplain},
+	[]Segment{actionApply, actionPortForward, actionExplain},
 	false,
 }
 
-/* Action tokens */
+/* Action segments */
 
-var actionGet = Token{
+var actionGet = Segment{
 	{Short: "g", Long: "get"},
 }
-var actionDelete = Token{
+var actionDelete = Segment{
 	{Short: "d", Long: "delete"},
 }
-var actionDescribe = Token{
+var actionDescribe = Segment{
 	{Short: "s", Long: "decribe"},
 }
-var actionEdit = Token{
+var actionEdit = Segment{
 	{Short: "e", Long: "edit"},
 }
-var actionExec = Token{
+var actionExec = Segment{
 	{Short: "x", Long: "exec"},
 }
-var actionLogs = Token{
+var actionLogs = Segment{
 	{Short: "l", Long: "logs"},
 }
-var actionApply = Token{
+var actionApply = Segment{
 	{Short: "a", Long: "apply"},
 }
-var actionPortForward = Token{
+var actionPortForward = Segment{
 	{Short: "p", Long: "port-forward"},
 }
-var actionExplain = Token{
+var actionExplain = Segment{
 	{Short: "ex", Long: "explain"},
 }
 
-/* Resource tokens */
+/* Resource segments */
 
-var resourcePod = Token{
+var resourcePod = Segment{
 	{Short: "p", Long: "pod"},
 }
-var resourceDeployment = Token{
+var resourceDeployment = Segment{
 	{Short: "d", Long: "deployment"},
 }
-var resourceService = Token{
+var resourceService = Segment{
 	{Short: "s", Long: "service"},
 }
-var resourceNode = Token{
+var resourceNode = Segment{
 	{Short: "n", Long: "node"},
 }
-var resourceIngress = Token{
+var resourceIngress = Segment{
 	{Short: "i", Long: "ingress"},
 }
-var resourceRole = Token{
+var resourceRole = Segment{
 	{Short: "r", Long: "role"},
 }
-var resourceRoleBinding = Token{
+var resourceRoleBinding = Segment{
 	{Short: "rb", Long: "rolebinding"},
 }
-var resourceClusterRole = Token{
+var resourceClusterRole = Segment{
 	{Short: "cr", Long: "clusterrole"},
 }
-var resourceClusterRoleBinding = Token{
+var resourceClusterRoleBinding = Segment{
 	{Short: "crb", Long: "clusterrolebinding"},
 }
 
-/* Option tokens */
+/* Option segments */
 
-var optionWatch = Token{
+var optionWatch = Segment{
 	{Short: "w", Long: "-w"},
 }
-var optionOutput = Token{
+var optionOutput = Segment{
 	{Short: "y", Long: "-o yaml"},
 	{Short: "j", Long: "-o json"},
 }
-var optionAllNamespaces = Token{
+var optionAllNamespaces = Segment{
 	{Short: "a", Long: "--all-namespaces"},
 }
-var optionAllDelete = Token{
+var optionAllDelete = Segment{
 	{Short: "a", Long: "--all"},
 }
-var optionAllNamespacesDelete = Token{
+var optionAllNamespacesDelete = Segment{
 	{Short: "A", Long: "--all-namespaces"},
 }
-var optionInteractive = Token{
+var optionInteractive = Segment{
 	{Short: "i", Long: "-it"},
 }
-var optionFollow = Token{
+var optionFollow = Segment{
 	{Short: "f", Long: "-f"},
 }
 
@@ -190,61 +190,61 @@ var optionFollow = Token{
 //==============================================================================
 
 func main() {
-	for _, s := range sets {
-		generate(s)
+	for _, suite := range suites {
+		generate(suite)
 	}
 }
 
-// Generate all aliases of a Set
-func generate(set Set) {
-	generateImpl(set, 0, []Token{})
+// Generate all aliases of a Suite
+func generate(suite Suite) {
+	generateImpl(suite, 0, []Segment{})
 }
-func generateImpl(set Set, i int, stack []Token) {
-	if i == len(set) {
+func generateImpl(suite Suite, i int, stack []Segment) {
+	if i == len(suite) {
 		return
 	}
-	// Group that can't be combined with each other
-	if !set[i].AllowCombinations {
-		for _, token := range set[i].Tokens {
+	// Group of non-combinable Segments
+	if !suite[i].AllowCombinations {
+		for _, token := range suite[i].Segments {
 			stackNew := append(stack, token)
 			for _, alternative := range getAlternatives(stackNew) {
 				printAlias(alternative)
 			}
-			generateImpl(set, i+1, stackNew)
+			generateImpl(suite, i+1, stackNew)
 		}
 	} else {
-		// Group that can be combined with each other. Create an alias for each per-
-		// mutation of each subset (size > 0) of the Group. Total number of aliases:
+		// Group of combinable Segments. All permutations of all subsets:
 		// https://www.wolframalpha.com/input/?i=sum%28n+choose+k%29+*+k%21%2Ck%3D1+to+n
-		for _, subset := range getSubsets(set[i].Tokens) {
+		for _, subset := range getSubsets(suite[i].Segments) {
 			for _, permutation := range getPermutations(subset) {
 				stackNew := append(stack, permutation...)
 				for _, alternative := range getAlternatives(stackNew) {
 					printAlias(alternative)
 				}
-				generateImpl(set, i+1, stackNew)
+				generateImpl(suite, i+1, stackNew)
 			}
 		}
 	}
 }
 
-func getAlternatives(tokens []Token) [][]Pair {
-	result := [][]Pair{}
+// Get final sequences of Tokens by expanding any mutually exclusive Tokens
+func getAlternatives(tokens []Segment) [][]Token {
+	result := [][]Token{}
+	c := make(chan []Token)
+	go getAlternativesImpl(tokens, 0, []Token{}, c)
 	// Number of expansions (for knowing how many values to receive from channel)
 	total := 1
 	for _, t := range tokens {
 		total = total * len(t)
 	}
-	c := make(chan []Pair)
-	go getAlternativesImpl(tokens, 0, []Pair{}, c)
 	for i := 0; i < total; i++ {
 		result = append(result, <-c)
 	}
 	return result
 }
-func getAlternativesImpl(tokens []Token, i int, stack []Pair, c chan []Pair) {
+func getAlternativesImpl(tokens []Segment, i int, stack []Token, c chan []Token) {
 	if i == len(tokens) {
-		stackCopy := make([]Pair, len(stack))
+		stackCopy := make([]Token, len(stack))
 		copy(stackCopy, stack)
 		c <- stackCopy
 	} else {
@@ -257,8 +257,8 @@ func getAlternativesImpl(tokens []Token, i int, stack []Pair, c chan []Pair) {
 // Aliases generated so far (for detecting name clashes)
 var aliases = map[string]string{}
 
-// Print a single alias definition given its sequence of Tokens
-func printAlias(pairs []Pair) {
+// Print a single alias definition given its sequence of Segments
+func printAlias(pairs []Token) {
 	alias, command := "k", "kubectl"
 	for _, pair := range pairs {
 		alias += pair.Short
@@ -277,21 +277,21 @@ func printAlias(pairs []Pair) {
 	fmt.Print(line)
 }
 
-// Get all subsets of size > 0 from a set (including the set itself)
-func getSubsets(set []Token) [][]Token {
-	c := make(chan []Token)
+// Get all subsets of size > 0 (including the set itself) of a set of Segments
+func getSubsets(set []Segment) [][]Segment {
+	c := make(chan []Segment)
 	go getSubsetsImpl(set, c)
-	result := [][]Token{}
+	result := [][]Segment{}
 	for subset := range c {
 		result = append(result, subset)
 	}
 	return result
 }
-func getSubsetsImpl(set []Token, c chan []Token) {
+func getSubsetsImpl(set []Segment, c chan []Segment) {
 	n := len(set)
 	// (2^n)-1 subsets represented by binary numbers i from 1 to (2^n)-1
 	for i := 1; i < 1<<uint(n); i++ {
-		subset := []Token{}
+		subset := []Segment{}
 		// Append elements at indices where the binary number i is 1
 		for j := 0; j < n; j++ {
 			if 1<<uint(j)&i > 0 {
@@ -303,11 +303,11 @@ func getSubsetsImpl(set []Token, c chan []Token) {
 	close(c)
 }
 
-// Get all permutations of a set
-func getPermutations(a []Token) [][]Token {
-	c := make(chan []Token)
+// Get all permutations of a set of Segments
+func getPermutations(a []Segment) [][]Segment {
+	c := make(chan []Segment)
 	go getPermutationsImpl(a, len(a), c)
-	result := [][]Token{}
+	result := [][]Segment{}
 	for permutation := range c {
 		result = append(result, permutation)
 	}
@@ -315,11 +315,11 @@ func getPermutations(a []Token) [][]Token {
 }
 
 // Heap's algorithm, see https://en.wikipedia.org/wiki/Heap%27s_algorithm
-func getPermutationsImpl(a []Token, k int, c chan []Token) {
+func getPermutationsImpl(a []Segment, k int, c chan []Segment) {
 	if k == 1 {
-		x := make([]Token, len(a))
-		copy(x, a)
-		c <- x
+		aCopy := make([]Segment, len(a))
+		copy(aCopy, a)
+		c <- aCopy
 	} else {
 		getPermutationsImpl(a, k-1, c)
 		for i := 0; i < k-1; i++ {
